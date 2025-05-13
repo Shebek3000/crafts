877467c910e074fbde1f8f313d3b8827d861c74e
Shebek3000 <shebek3000@gmail.com>
2025-05-13 21:52:35 +0300

    Delete JavaScript directory

--- JavaScript/hoverswitch1.js
+++
@@ -1,5 +0,0 @@
-function smiana(n)
-{
-	if(n==0) document.images.kart.src="Images/durvonazhivota.png";
-	if(n==1) document.images.kart.src="Images/durvonazhivotapython.png";
-}
--- JavaScript/hoverswitch2.js
+++
@@ -1,5 +0,0 @@
-function smianaa(n)
-{
-	if(n==0) document.images.karta.src="Images/katanica1.png";
-	if(n==1) document.images.karta.src="Images/katanica1python.png";
-}
